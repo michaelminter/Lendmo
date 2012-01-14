11f8d66da55754ba80c71dc325d1260c011fd7bb
Joseph Zhong <pandotree@gmail.com>
2012-01-14 04:49:20 -0500

    idk what's going on

--- app/models/user.rb
+++ app/models/user.rb
@@ -79,5 +79,25 @@ class User < ActiveRecord::Base
     end
   end
   
+<<<<<<< HEAD
+>>>>>>> 330373e4cfca93dbe592ad3087ce3f1e6483b474
+=======
+  # Find the user trying to sign in, or create a new one if they're new
+  def self.find_or_create(auth_hash)
+    existing = User.where("fb_id = ?", auth_hash[:uid]).first
+    
+    if existing.nil?
+      User.create(
+        :first_name => auth_hash[:info][:first_name],
+        :last_name => auth_hash[:info][:last_name],
+        :email => auth_hash[:info][:email],
+        :url => auth_hash[:info][:urls][:Facebook],
+        :fb_id => auth_hash[:uid]
+      )
+    else
+      existing
+    end
+  end
+  
 >>>>>>> 330373e4cfca93dbe592ad3087ce3f1e6483b474
 end
