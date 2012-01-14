0db64498939c6f912d70f6ab0aa03445e114512e
Joseph Zhong <pandotree@gmail.com>
2012-01-14 05:48:06 -0500

    added islending column to event

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120114092803) do
+ActiveRecord::Schema.define(:version => 20120114104450) do
 
   create_table "comments", :force => true do |t|
     t.string   "content"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(:version => 20120114092803) do
     t.integer  "item_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.boolean  "islending"
   end
 
   create_table "items", :force => true do |t|
