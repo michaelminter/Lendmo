60a43076679d46d7e8784fe6ad178e7abf26cc1d
Adi Dahiya <adidas006@gmail.com>
2012-01-13 19:20:23 -0500

    Created initial rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lendmo::Application.config.secret_token = 'c4fb21bc17d21c5386a0351feef51969083c046eb8af3fed4df8daeed471dbc197f50cf42d97f61706193cde6e970066477e80e2c4788f7b51315cd55971bf56'
