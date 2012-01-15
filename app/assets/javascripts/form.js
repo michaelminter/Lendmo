cf119045e570e21a861a426089ac1d03d6de0976
Joy <jming@college.harvard.edu>
2012-01-14 20:30:07 -0500

    trying to fix merge conflicts

--- app/assets/javascripts/form.js
+++ app/assets/javascripts/form.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
     var lend_form = $("#lend-form");
     var borrow_form = $("#borrow-form");
+    var INVALID = "invalid";
 
     // hide our forms before the user clicks the buttons, and disable
     // the submit buttons
@@ -14,69 +15,48 @@ $(document).ready(function() {
     // action performed when user clicks lend button
     // The form box will expand during this action
     $("#lend").click(function() {
-        if (  lend_form.css("display") === "none" &&
-            borrow_form.css("display") === "none") {
-
-            lend_form.slideDown();
-        }
-        else {
-            lend_form.show();
-        }
+        lend_form.fadeIn();
         borrow_form.hide();
     });
 
     // borrow form box expands when user clicks borrow button
     $("#borrow").click(function() {
-        if (  lend_form.css("display") === "none" &&
-            borrow_form.css("display") === "none") {
-
-            borrow_form.slideDown();
-        }
-        else {
-            borrow_form.show();
-        }
+        borrow_form.fadeIn();
         lend_form.hide();
     });
 
     // When the user submits the form, validate input first
-    $("#lend-form :submit").submit(function() {
+    $("#lend-submit").click(function(e) {
         var lend_friend, lend_item;
-        lend_friend = $("#lend .friend");
-        borrow_friend = $("#borrow .friend");
+        lend_friend = $("#lend-form .chzn-select");
+        lend_item = $("#lend-form .item");
 
-        lend_friend.removeClass("invalid");
-        lend_item.removeClass("invalid");
+        lend_friend.removeClass(INVALID);
+        lend_item.removeClass(INVALID);
 
         if (lend_friend.val() === "") {
-            lend_friend.addClass("invalid");
-            return false;
+            lend_friend.addClass(INVALID);
+            e.preventDefault();
+            return;
         }
 
         if (lend_item.val() === "") {
-            lend_item.addClass("invalid)");
-            return false;
+            lend_item.addClass(INVALID);
+            e.preventDefault();
+            return;
         }
 
-        return true;
     });
 
-    $("#borrow-form :submit").submit(function() {
-        var borrow_friend, borrow_item;
-
-        borrow_friend = $("#borrow .friend");
-        if (borrow_friend.val() === "") {
-            borrow_friend.addClass("invalid");
-            return false;
-        }
+    $("#borrow-submit").click(function(e) {
+        var borrow_item = $("#borrow-form .item");
 
         if (borrow_item.val() === "") {
-            borrow_item.addClass("invalid)");
-            return false;
+            borrow_item.addClass(INVALID);
+            e.preventDefault();
+            return;
         }
 
-        borrow_friend.removeClass("invalid");
-        borrow_item.removeClass("invalid");
-
-        return true;
+        borrow_item.removeClass(INVALID);
     });
 });
